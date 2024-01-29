c85b5ef08e69329db3d4a7b0dff58e68611dbd2f
Cedric Blanchard <blanchard.ced@gmail.com>
2024-01-29 10:30:18 +0100

    login page component ok

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,12 +9,17 @@ function Login({ username, setUsername }) {
     setUsername(e.target.value);
   }
 
+  function handleSubmit() {
+    setUsername('');
+    alert(`Bonjour ${username}`);
+  }
+
   return (
     <>
       <h1>Bienvenue chez nous !</h1>
       <h2>Connectez-vous</h2>
-      <form onSubmit={setUsername}>
-        <input type='text' name='username' id='username' value={username} onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+        <input type='text' name='username' id='username' value={username} placeholder='Entrez votre prénom...' onChange={handleChange} required />
         <button>Accédez à votre espace</button>
       </form>
     </>
