0ff8e6fb2c6cef67c722cc3b68f1301884e33747
Cedric Blanchard <blanchard.ced@gmail.com>
2024-01-29 13:20:38 +0100

    [feat]update orderpage

--- src/pages/OrderPage.jsx
+++ src/pages/OrderPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-function OrderPage() {
-  return <div>OrderPage</div>;
+function OrderPage({ username, setUsername }) {
+  function handleClick() {
+    setUsername('');
+  }
+
+  return (
+    <>
+      <h1>Bienvenue {username}</h1>
+      <button onClick={handleClick}>Déconnexion</button>
+    </>
+  );
 }
 
 export default OrderPage;
