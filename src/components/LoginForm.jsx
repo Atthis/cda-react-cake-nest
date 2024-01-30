bdc774b41ec59f735e1c9bfe9887868b7776fd92
Cedric Blanchard <blanchard.ced@gmail.com>
2024-01-30 10:16:32 +0100

    add styled-component+style first part login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import { Form, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { theme } from '../assets';
 
-function LoginForm({ username, setUsername }) {
-  const navigateTo = useNavigate();
+const Form = styled.form`
+  width: 100%;
 
-  function handleChange(e) {
-    e.preventDefault();
-    setUsername(e.target.value);
+  input,
+  button {
+    display: block;
+    width: 100%;
+    height: 50px;
+    margin-block: ${theme.spacing.md};
+    padding-block: ${theme.spacing.lg};
+    border: none;
+    border-radius: ${theme.borderRadius.round};
   }
 
-  function handleSubmit() {
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${theme.colors.primary};
+    font-size: ${theme.fonts.size.P3};
+    font-weight: 700;
+    color: ${theme.colors.white};
+  }
+`;
+
+function LoginForm({ setUsername }) {
+  const navigateTo = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setUsername(e.target.username.value);
     navigateTo('/commandes');
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type='text' name='username' id='username' value={username} placeholder='Entrez votre prénom...' onChange={handleChange} required autoComplete='name' />
-      <button>Accédez à votre espace</button>
-    </form>
+    <Form onSubmit={handleSubmit}>
+      <input type='text' name='username' id='username' placeholder='Entrez votre prénom...' required autoComplete='name' />
+      <button>Mon espace</button>
+    </Form>
   );
 }
 
